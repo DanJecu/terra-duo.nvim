623f5c20183aa0217d0437c5ea40d6ddb4f16b65
DanJecu <danjecu@outlook.com>
2026-04-22 12:51:59 +0200

    fix preview EndOfBuffer via winhighlight autocmd on snacks filetypes

--- lua/solarized-duo/init.lua
+++ lua/solarized-duo/init.lua
@@ -22,6 +22,25 @@ function M.load(variant)
   for group, spec in pairs(hl) do
     vim.api.nvim_set_hl(0, group, spec)
   end
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("SolarizedDuoSnacks", { clear = true }),
+    pattern = {
+      "snacks_picker_list",
+      "snacks_picker_input",
+      "snacks_picker_preview",
+      "snacks_dashboard",
+    },
+    callback = function()
+      local extra = "EndOfBuffer:SnacksPicker,Normal:SnacksPicker,LineNr:SnacksPicker,SignColumn:SnacksPicker"
+      local cur = vim.wo.winhighlight
+      if cur == "" then
+        vim.wo.winhighlight = extra
+      elseif not cur:find("EndOfBuffer:SnacksPicker", 1, true) then
+        vim.wo.winhighlight = cur .. "," .. extra
+      end
+    end,
+  })
 end
 
 return M
